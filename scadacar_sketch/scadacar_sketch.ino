f3bc0ce1eccaa080e41aa716c39de21899db8072
ruygarcia <rrgarciach@gmail.com>
2014-11-03 01:59:49 -0600

    Added TinyGPS functionality. The GPS data is sended though the Bluetooth :)

--- scadacar_sketch/scadacar_sketch.ino
+++ scadacar_sketch/scadacar_sketch.ino
@@ -4,29 +4,37 @@
  Created: July 22, 2014
  By: Ruy García
  */
- 
+
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
+// Bluetooth Serial parameters:
 SoftwareSerial bt(5,4);
 char btt = '0';
 char srl = '0';
+
+// TinyGPS parameters:
+static const int RXPin = 2, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps; // The TinyGPS++ object
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup()
 {
   Serial.begin(115200);
   bt.begin(9600);
+  ss.begin(GPSBaud);
 }
 
 void loop()
 {
-  sendMessage("message of LOG test");
-  delay(1000);
+  readGPS();
 }
 
 void sendMessage(String message)
 {
-  Serial.print("Sending message: ");
-  Serial.println(message);
+  //Serial.print("Sending message: ");
+  //Serial.println(message);
   bt.println(message);
 }
 
@@ -42,4 +50,44 @@ void readMessage()
       delay(50);
     }
   }
+}
+
+void readGPS()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  String strValue;
+  char charBuf[15];
+  if (gps.location.isValid())
+  {
+    // This function is thanks to http://dereenigne.org/arduino/arduino-float-to-string
+    dtostrf(gps.location.lat(), 12, 6, charBuf);
+    strValue += charBuf;
+    //Serial.print(strValue);
+    strValue += ",";
+    //Serial.print(F(","));
+    dtostrf(gps.location.lng(), 12, 6, charBuf);
+    strValue += charBuf;
+    //Serial.print(charBuf);
+  }
+  else
+  {
+    //Serial.print(F("INVALID"));
+    strValue += "INVALID";
+  }
+
+  //Serial.println();
+  sendMessage(strValue);
 }
