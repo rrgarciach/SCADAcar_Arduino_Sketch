9e20c9cfd799fd6f3f1f3922b531326179980e6c
ruygarcia <rrgarciach@gmail.com>
2014-11-11 17:37:33 -0600

    Fixed sending messages functions and OBD constants.

--- scadacar_sketch/scadacar_sketch.ino
+++ scadacar_sketch/scadacar_sketch.ino
@@ -24,56 +24,56 @@ int delayTime = 10; // delay time (in seconds) between messages.
 
 SoftwareSerial obd(8,7);
 // Mode 1 PIDs
-#define PID_ENGINE_LOAD 0104
-#define PID_COOLANT_TEMP 0105
-#define PID_SHORT_TERM_FUEL_TRIM_1 0106
-#define PID_LONG_TERM_FUEL_TRIM_1 0107
-#define PID_SHORT_TERM_FUEL_TRIM_2 0108
-#define PID_LONG_TERM_FUEL_TRIM_2 0109
-#define PID_FUEL_PRESSURE 010A
-#define PID_INTAKE_MAP 010B
-#define PID_RPM 010C
-#define PID_SPEED 010D
-#define PID_TIMING_ADVANCE 010E
-#define PID_INTAKE_TEMP 010F
-#define PID_MAF_FLOW 0110
-#define PID_THROTTLE 0111
-#define PID_AUX_INPUT 011E
-#define PID_RUNTIME 011F
-#define PID_DISTANCE_WITH_MIL 0121
-#define PID_COMMANDED_EGR 012C
-#define PID_EGR_ERROR 012D
-#define PID_COMMANDED_EVAPORATIVE_PURGE 012E
-#define PID_FUEL_LEVEL 012F
-#define PID_WARMS_UPS 0130
-#define PID_DISTANCE 0131
-#define PID_EVAP_SYS_VAPOR_PRESSURE 0132
-#define PID_BAROMETRIC 0133
-#define PID_CATALYST_TEMP_B1S1 013C
-#define PID_CATALYST_TEMP_B2S1 013D
-#define PID_CATALYST_TEMP_B1S2 013E
-#define PID_CATALYST_TEMP_B2S2 013F
-#define PID_CONTROL_MODULE_VOLTAGE 0142
-#define PID_ABSOLUTE_ENGINE_LOAD 0143
-#define PID_RELATIVE_THROTTLE_POS 0145
-#define PID_AMBIENT_TEMP 0146
-#define PID_ABSOLUTE_THROTTLE_POS_B 0147
-#define PID_ABSOLUTE_THROTTLE_POS_C 0148
-#define PID_ACC_PEDAL_POS_D 0149
-#define PID_ACC_PEDAL_POS_E 014A
-#define PID_ACC_PEDAL_POS_F 014B
-#define PID_COMMANDED_THROTTLE_ACTUATOR 014C
-#define PID_TIME_WITH_MIL 014D
-#define PID_TIME_SINCE_CODES_CLEARED 014E
-#define PID_ETHANOL_FUEL 0152
-#define PID_FUEL_RAIL_PRESSURE 0159
-#define PID_HYBRID_BATTERY_PERCENTAGE 015B
-#define PID_ENGINE_OIL_TEMP 015C
-#define PID_FUEL_INJECTION_TIMING 015D
-#define PID_ENGINE_FUEL_RATE 015E
-#define PID_ENGINE_TORQUE_DEMANDED 0161
-#define PID_ENGINE_TORQUE_PERCENTAGE 0162
-#define PID_ENGINE_REF_TORQUE 0163
+#define PID_ENGINE_LOAD "01041"
+#define PID_COOLANT_TEMP "01051"
+#define PID_SHORT_TERM_FUEL_TRIM_1 "01061"
+#define PID_LONG_TERM_FUEL_TRIM_1 "01071"
+#define PID_SHORT_TERM_FUEL_TRIM_2 "01081"
+#define PID_LONG_TERM_FUEL_TRIM_2 "01091"
+#define PID_FUEL_PRESSURE "010A1"
+#define PID_INTAKE_MAP "010B1"
+#define PID_RPM "010C1"
+#define PID_SPEED "010D1"
+#define PID_TIMING_ADVANCE "010E1"
+#define PID_INTAKE_TEMP "010F1"
+#define PID_MAF_FLOW "01101"
+#define PID_THROTTLE "01111"
+#define PID_AUX_INPUT "011E1"
+#define PID_RUNTIME "011F1"
+#define PID_DISTANCE_WITH_MIL "01211"
+#define PID_COMMANDED_EGR "012C1"
+#define PID_EGR_ERROR "012D1"
+#define PID_COMMANDED_EVAPORATIVE_PURGE "012E1"
+#define PID_FUEL_LEVEL "012F1"
+#define PID_WARMS_UPS "01301"
+#define PID_DISTANCE "01311"
+#define PID_EVAP_SYS_VAPOR_PRESSURE "01321"
+#define PID_BAROMETRIC "01331"
+#define PID_CATALYST_TEMP_B1S1 "013C1"
+#define PID_CATALYST_TEMP_B2S1 "013D1"
+#define PID_CATALYST_TEMP_B1S2 "013E1"
+#define PID_CATALYST_TEMP_B2S2 "013F1"
+#define PID_CONTROL_MODULE_VOLTAGE "01421"
+#define PID_ABSOLUTE_ENGINE_LOAD "01431"
+#define PID_RELATIVE_THROTTLE_POS "01451"
+#define PID_AMBIENT_TEMP "01461"
+#define PID_ABSOLUTE_THROTTLE_POS_B "01471"
+#define PID_ABSOLUTE_THROTTLE_POS_C "01481"
+#define PID_ACC_PEDAL_POS_D "01491"
+#define PID_ACC_PEDAL_POS_E "014A1"
+#define PID_ACC_PEDAL_POS_F "014B1"
+#define PID_COMMANDED_THROTTLE_ACTUATOR "014C1"
+#define PID_TIME_WITH_MIL "014D1"
+#define PID_TIME_SINCE_CODES_CLEARED "014E1"
+#define PID_ETHANOL_FUEL "01521"
+#define PID_FUEL_RAIL_PRESSURE "01591"
+#define PID_HYBRID_BATTERY_PERCENTAGE "015B1"
+#define PID_ENGINE_OIL_TEMP" 015C1"
+#define PID_FUEL_INJECTION_TIMING "015D1"
+#define PID_ENGINE_FUEL_RATE "015E1"
+#define PID_ENGINE_TORQUE_DEMANDED "01611"
+#define PID_ENGINE_TORQUE_PERCENTAGE "01621"
+#define PID_ENGINE_REF_TORQUE "01631"
 
 void setup()
 {
@@ -96,7 +96,7 @@ void loop()
   delay(1000);
   send_OBD_cmd("0140");     //send 0140, retrieve available pid's 40-??
   delay(1000);
-  send_OBD_cmd("010C1");    //send 010C1, RPM cmd
+  send_OBD_cmd(PID_RPM);    //send 010C1, RPM cmd
   delay(1000);
 }
 
@@ -110,28 +110,29 @@ void sendMessage(String message)
 
 void readMessage()
 {
-  if (bt.available())
-  {
-    String command; //string to store entire command line
-    while (bt.available())
-    {
-      msg = bt.read();
+  if (bt.available() > 0) {
+    String msg; //string to store entire command line
+    while (bt.available()) {
+      msg += bt.read();
       Serial.print(msg);
       delay(50);
     }
   }
+  bt.flush();
 }
 
-void readOBD() {
-  while (obd.available() > 0) {
-    String command; //string to store entire command line
+void readOBD()
+{
+  if (obd.available() > 0) {
+    String msg; //string to store entire command line
     delay(1000); // wait 1 sec to give time enough for the serial to receive all the stream
-    if (obd.available()) {
-      msg = obd.read();
+    while (obd.available()) {
+      msg += obd.read();
       Serial.print(msg);
       delay(50);
     }
   }
+  obd.flush();
 }
 
 void readGPS()
@@ -181,11 +182,8 @@ void displayInfo()
   }
 }
 
-void send_OBD_cmd(char *obd_cmd){
-  obd.print(obd_cmd);
-  obd.print("\r");
-  while (obd.available() <= 0);
-    while(obd.available() > 0){
-      recvChar = obd.read();
-  }
+void send_OBD_cmd(char *obd_cmd)
+{
+  obd.println(obd_cmd);
+  readOBD();
 }
